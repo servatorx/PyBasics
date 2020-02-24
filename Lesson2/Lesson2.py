e59ff58b8a9f32a3d583c023cb75e226c23518cd
Artem Valishin <av@sent.com>
2020-02-24 10:57:53 +0300

    Задание 1

--- Lesson2/Lesson2.py
+++ Lesson2/Lesson2.py
@@ -1,1 +1,7 @@
 print("--------- Задание 1---------")
+my_list = [12,(2.6,"abc"),'string_here',True,{"a":6,"b":11.2}]
+print("Переменная my_list типа: " + str(type(my_list)))
+
+for i in my_list:
+    text = "Элемент " + str(i) + " имеет тип: " + str(type(i))
+    print(text)
