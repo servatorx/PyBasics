c84509ad90f3af071017ff7ea9369759eca0de19
Artem Valishin <av@sent.com>
2020-03-02 14:28:46 +0300

    HW3 Задание 2

--- Lesson3/Lesson3-1.py
+++ Lesson3/Lesson3-1.py
@@ -1,1 +1,13 @@
-#test
+def division(numb1, numb2):
+    try:
+        n1 = int(numb1)
+        n2 = int(numb2)
+        result = n1 / n2
+    except ZeroDivisionError:
+        return "Ошибка деления на 0"
+    except ValueError:
+        return "Ошибка формата числа"
+    return result
+num1 = input("Введите первое число: ")
+num2 = input("Введите второе число: ")
+print(division(num1,num2))
---
+++ Lesson3/Lesson3-2.py
@@ -0,0 +1,4 @@
+def person_info(name, surname, year, city, email, phone):
+    return f"Имя: {name}, Фамилия: {surname}, Год рождения: {year}, город проживания: {city}, email: {email}, телефон: {phone}"
+
+print(person_info(name = "Саша", surname="Петров", year="1990", city="Рига", email="aa@aa.lv", phone="+3710011222"))
