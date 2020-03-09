464a86f2b057131284a0658d64d010c5c756517b
Artem Valishin <av@sent.com>
2020-03-09 14:36:15 +0300

    HW 5-3

--- Lesson5/Lesson5-3.py
+++ Lesson5/Lesson5-3.py
@@ -2,8 +2,6 @@ try:
     f_obj = open("salary.txt", "r", encoding='UTF8')
     strings_list = f_obj.readlines()
     f_obj.close()
-    #print("Количество строк: " + str(len(strings_list)))
-
     i = 1
     salary_sum = 0
     for lines_ in strings_list:
@@ -11,7 +9,6 @@ try:
         salary_sum += float(fam_zp[1])
         if float(fam_zp[1]) < 20000:
             print(fam_zp[0])
-        #print("Количество слов в строке " + str(i) + ": " + str(len(words_.split())))
         i += 1
 
 except IOError:
